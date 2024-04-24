2963dcc0a141a8fbc98eb402902e55520bbdd88a
Ashwin <ashwinkv.akv@gmail.com>
2024-04-24 12:27:45 +0530

    update cors

--- src/newsService.ts
+++ src/newsService.ts
@@ -2,7 +2,7 @@ const API_KEY = import.meta.env.VITE_NEWS_API;
 
 export const fetchNews = async (category: string): Promise<any[]> => {
   const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${API_KEY}`
+    `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${API_KEY}`
   );
   const data = await response.json();
 
@@ -10,7 +10,7 @@ export const fetchNews = async (category: string): Promise<any[]> => {
 };
 export const searchNews =async (query: string): Promise<any[]> => {
   const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=in&q=${query}&apiKey=${API_KEY}`
+    `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=in&q=${query}&apiKey=${API_KEY}`
   );
   const data = await response.json();
   console.log(data);
